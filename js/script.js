0e18638eb2ffb802ee0eda6610d28838272f699a
marimanger <59840447+marimanger@users.noreply.github.com>
2021-03-13 13:06:37 +0000

    добавила карту

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ close.addEventListener("click", function (evt) {
 
 window.addEventListener("keydown", function (evt) {
 
-  if(evt.keyCode === 27) {
+  if (evt.keyCode === 27) {
     if (popup.classList.contains("modal-appear")) {
       evt.preventDefault();
       popup.classList.remove("modal-appear");
@@ -29,4 +29,41 @@ window.addEventListener("keydown", function (evt) {
   }
 
 });
+
+// The ymaps.ready() function will be called when
+// all the API components are loaded and the DOM tree is generated.
+ymaps.ready(init);
+function init() {
+  // Creating the map.
+  let siteMap = new ymaps.Map("map", {
+
+    center: [59.939113, 30.321420],
+    // Zoom level. Acceptable values:
+    // from 0 (the entire world) to 19.
+    zoom: 17
+  });
+  siteMap.controls.remove("geololocationControl");
+  siteMap.controls.remove("rulerControl");
+  siteMap.controls.remove("zoomControl");
+  siteMap.controls.remove("searchControl");
+  siteMap.controls.remove("trafficControl");
+  siteMap.behaviors.disable("scrollZoom");
+
+
+
+    myPlacemark = new ymaps.Placemark(siteMap.getCenter(), {
+      hintContent: "NЁRDS DESIGN STUDIO, 191186, Санкт-Петербург, ул. Б. Конюшенная, д. 19/8"
+    }, {
+      iconLayout: "default#image",
+      iconImageHref: "img/map-marker.svg",
+      iconImageSize: [231, 190],
+      iconImageOffset: [110, -125]
+    });
+
+
+  siteMap.geoObjects.add(myPlacemark);
+
+}
+
+
 
